8213ff2893735da0afa01d63df3a1a514fbb88e9
parthshah05 <parth.babu05@gmail.com>
2020-12-30 10:38:45 +0530

    master collections

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a6a420ccc3d535a2b96e75df4b93dc77'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2b6f1cda-1736-42c4-8a33-6c880b5df4ef'
+};
